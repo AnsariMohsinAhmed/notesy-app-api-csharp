248a8a8b952956e75dda3495e8535d42b0626902
Ansari Mohsin Ahmed <mohsinahmedansari99@gmail.com>
2025-10-13 14:47:00 +0530

    Updated - Note model added | Working on CRUD endpoints

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using notesy_api_c_sharp.Models;
+
+namespace notesy_api_c_sharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private List<Note> Notes =
+        [
+            new Note { ID = 1, Text = "First note"},
+            new Note { ID = 2, Text = "Second note"}
+        ];
+
+        // get all notes
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Note>> GetAll()
+        {
+            return Ok(Notes);
+        }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace notesy_api_c_sharp.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public string? Text { get; set; }
+    }
+}
