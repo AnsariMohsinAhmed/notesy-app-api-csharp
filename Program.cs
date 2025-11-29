4626990c8b746c24130d3011dfcef0a5ea380d0d
Mohsin Ahmed Ansari <Mohsin@cloudwrx.in>
2025-11-29 16:21:46 +0530

    temporarily removed ping background service

--- Program.cs
+++ Program.cs
@@ -23,8 +23,8 @@ var env = builder.Environment.EnvironmentName;
 var connectionString = builder.Configuration["ProductionDBUrl"];
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-builder.Services.AddHostedService<PingBackGroundService>();
-builder.Services.AddHttpClient();
+//builder.Services.AddHostedService<PingBackGroundService>();
+//builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
